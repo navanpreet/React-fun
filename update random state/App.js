88fc02980e8836d1442beb1df996e23767f23f54
Navan <navanpreet09@gmail.com>
2017-11-13 23:49:30 -0800

    Add files via upload

---
+++ update random state/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      instructors: [
+        {
+          name: 'Tim',
+          hobbies: ['sailing', 'react']
+        }, {
+          name: 'Matt',
+          hobbies: ['math', 'd3']
+        }, {
+          name: 'Colt',
+          hobbies: ['css', 'hiking']
+        }, {
+          name: 'Elie',
+          hobbies: ['music', 'es2015']
+        }
+      ]
+    };
+
+    setTimeout(() => {
+      let instructorIndex = Math.floor(Math.random() * this.state.instructors.length);
+      let hobbyIndex = Math.floor(Math.random() * this.state.instructors[instructorIndex].hobbies.length);
+      let instructors = this.state.instructors.map((instructor, index) => {
+        if(index === instructorIndex){
+          instructor.hobbies.splice(hobbyIndex, 1);
+          return {name: instructor.name, hobbies: instructor.hobbies};
+        } else {
+          return instructor;
+        }
+      });
+      this.setState({instructors});
+    }, 5000);
+  }
+  render() {
+    const instructors = this.state.instructors.map((instructor, index) => (
+      <li key={index}>
+        <h3>{instructor.name}</h3>
+        <h4>Hobbies: {instructor.hobbies.join(", ")}</h4>
+      </li>
+    ));
+    return (
+      <div className="App">
+        <ul>
+          {instructors}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
